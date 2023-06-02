01dfc16a164caa2d3cdd7ee886678f2a27849b2b
Emmanuel Gonzales <emmanuelgonzales2002@gmail.com>
2023-06-01 20:59:40 -0500

    startup

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3002;
+
+app.get('/', (req, res, next) => {
+  res.status(200).send('Hello World');
+});
+
+app.get('/hello', (req, res, next) => {
+  let name = req.query.name
+  res.status(200).send(`Hello ${name}`);
+});
+
+
+app.listen(PORT, () => console.log('Server Running on Port:', PORT));
